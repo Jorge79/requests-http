362263630c20c40ebb5572d90ae7274e143fc862
Jorge Francisco <Jorge7.9@hotmail.com>
2022-06-08 21:16:21 -0300

    Http: listar registros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'cursos',
+  },
+  {
+    path: 'cursos',
+    loadChildren: () =>
+      import('../app/cursos/cursos.module').then((mod) => mod.CursosModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
