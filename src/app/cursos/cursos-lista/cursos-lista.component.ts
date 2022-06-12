3b9568153382d90fc80d3a4dbc3ce4917e2a7c10
Jorge Francisco <Jorge7.9@hotmail.com>
2022-06-12 19:43:03 -0300

    Http Delete

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { catchError, empty, Observable, Subject } from 'rxjs';
@@ -6,6 +6,7 @@ import { catchError, empty, Observable, Subject } from 'rxjs';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
 import { AlertModalService } from 'src/app/shared/alert-modal/alert-modal.service';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -19,11 +20,17 @@ export class CursosListaComponent implements OnInit {
   cursos$!: Observable<Curso[]>;
   error$ = new Subject<boolean>();
 
+  cursoSelecionado: Curso;
+
+  deleteModalRef!: BsModalRef;
+  @ViewChild('deleteModal') deleteModal;
+
   constructor(
     private service: CursosService,
     private alertService: AlertModalService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private modalService: BsModalService
   ) {}
 
   ngOnInit() {
@@ -54,5 +61,26 @@ export class CursosListaComponent implements OnInit {
 
   onEdit(id: number | null) {
     this.router.navigate(['editar', id], { relativeTo: this.route });
+  }
+
+  onDelete(curso) {
+    this.cursoSelecionado = curso;
+    this.deleteModalRef = this.modalService.show(this.deleteModal, {
+      class: 'modal-sm',
+    });
+  }
+
+  onConfirmDelete() {
+    this.service.remove(this.cursoSelecionado.id).subscribe(
+      (success) => this.onRefresh(),
+      (error) =>
+        this.alertService.showAlertDanger(
+          'Erro ao carregar cursos, tente novamente mais tarde'
+        )
+    );
+  }
+
+  onDeclineDelete() {
+    this.deleteModalRef.hide();
   }
 }
